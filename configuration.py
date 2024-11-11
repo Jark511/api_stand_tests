21981cfdfac2675213b51af4e26968130d7e6692
Jorge Bernal <alfbern16@hotmail.com>
2024-11-11 17:41:22 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-8a973b1d-2d9a-4a0d-96a0-7f969c12e01a.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
